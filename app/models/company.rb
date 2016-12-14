cd6540e8699886110208516f25776286d11a01bc
Florian Kolb <florian.kolb@mni.thm.de>
2016-12-14 10:14:28 +0100

    completed associations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ApplicationRecord
   has_many :workdays
   has_many :projects
+  belongs_to :user
   validates :title, presence: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ApplicationRecord
+  has_many  :workdays
   belongs_to :user
-  #belongs_to :company
+  belongs_to :company
   validates :title, presence: true
 end
